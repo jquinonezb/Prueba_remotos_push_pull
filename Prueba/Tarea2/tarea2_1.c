e083a88102acee9436ffb08a4164b1bb19d11023
Jose Leonardo Quiñonez <leonardoquinonez98@gmail.com>
2021-09-11 14:26:48 -0500

    se mejoró la parte de la fecha de nacimiento

--- Prueba/Tarea2/tarea2_1.c
+++ Prueba/Tarea2/tarea2_1.c
@@ -9,8 +9,10 @@ void main (void)
 	int num_nom=0;
 	char nom1[TAM_MAX], nom2[TAM_MAX], c = 0;
 	char ap_pat[TAM_MAX], ap_mat[TAM_MAX], dia[TAM_MD], mes[TAM_MD];
-	char year[TAM_FE];
-  	printf("Bienvenido al sistema de captura de datos para CURP\n");
+	char year[TAM_AN];
+  	char numero=165;
+	printf("%c\n",numero);
+	printf("Bienvenido al sistema de captura de datos para CURP\n");
 	printf("¿Cuántos nombres tienes? (1 o 2):\n");
 	scanf("%d", &num_nom);
  if (num_nom == 1)
@@ -24,12 +26,12 @@ void main (void)
 	int cont = 0;
 	for(int a = 0; a<=TAM_MAX; a++)
 	{
-		if (nom1[a]>=65 && nom1[a]<=90 || nom1[a]==165)
+		if (nom1[a]>=65 && nom1[a]<=90)
 		{
 			cont++;
 			printf("%d \n", cont);
 		}
-		else if(nom1[a] >= 97 && nom1[a]<= 122 || nom1[a]==164 )
+		else if(nom1[a] >= 97 && nom1[a]<= 122 || nom1[a]==164)
 		{		
 			printf("Tu nombre no está completamente en mayúsculas\n");
 			break;
@@ -54,12 +56,12 @@ void main (void)
 	int cont = 0;
 	for(int a = 0; a<=TAM_MAX; a++)
 	{
-		if (nom1[a]>=65 && nom1[a]<=90 nom1[a]==165)
+		if (nom1[a]>=65 && nom1[a]<=90)
 		{
 			cont++;
 			printf("%d \n", cont);
 		}
-		else if(nom1[a]>= 97 && nom1[a]<= 122 nom1[a]==164)
+		else if(nom1[a]>= 97 && nom1[a]<= 122 || nom1[a]==164)
 		{		
 		     printf("Tu nombre no está completamente en mayúsculas\n");
 		     break;
@@ -73,7 +75,7 @@ void main (void)
   	} while(c==0);
 
 	
-	char c = 0;
+	c = 0;
  do
  {
 	printf("Por favor introduce tu segundo nombre en mayúsculas: \n");
@@ -82,7 +84,7 @@ void main (void)
 	int cont2 = 0;
 	for(int a = 0; a<=TAM_MAX; a++)
 	{
-		if (nom2[a]>=65 && nom2[a]<=90 || nom2[a]==165)
+		if (nom2[a]>=65 && nom2[a]<=90)
 		{
 			cont2++;
 			printf("%d \n", cont2);
@@ -101,20 +103,21 @@ void main (void)
   	} while(c==0);
 }
 	/*APELLIDO PATERNO*/
-	char c = 0;
+	c = 0;
 	do
  {
 	printf("Por favor introduce tu apellido paterno en mayúsculas: \n");
+	printf("Si tu apellido tiene eñe por favor sustituyela con una X\n");
 	scanf("%s", ap_pat);
 	int cont3 = 0;
 	for(int a = 0; a<=TAM_MAX; a++)
 	{
-		if (ap_pat[a]>=65 && ap_pat[a]<=90 || ap_pat[a]==165)
+		if (ap_pat[a]>=65 && ap_pat[a]<=90)
 		{
 			cont3++;
 			printf("%d \n", cont3);
 		}
-		else if(ap_pat[a]>= 97 && ap_pat[a]<= 122 || ap_pat[a]==165)
+		else if(ap_pat[a]>= 97 && ap_pat[a]<= 122 || ap_pat[a]==164)
 		{		
 		 printf("Tu apellido no está completamente en mayúsculas\n");
 		 break;
@@ -128,15 +131,16 @@ void main (void)
   	} while(c==0);
 	
 	/*APELLIDO MATERNO*/
-	char c = 0;
+	c = 0;
 	do
  {
         printf("Por Favor introduce tu apellido materno en mayúsculas: \n");
+	printf("Si tu apellido tiene eñe por favor sustituyela con una X\n");
 	scanf("%s", ap_mat);
 		int cont4 = 0;
 	for(int a = 0; a<=TAM_MAX; a++)
 	{
-		if (ap_mat[a]>=65 && ap_mat[a]<=90 || ap_mat[a]==165)
+		if (ap_mat[a]>=65 && ap_mat[a]<=90)
 		{
 			cont4++;
 			printf("%d \n", cont4);
@@ -156,44 +160,45 @@ void main (void)
 /* FECHA DE NACIMIENTO*/
 	/*DIA DE NACIMIENTO*/
 	printf("Ahora ingrese su fecha de nacimiento (el formato será dd/mm/aaaa)\n");
-	d==0;
+	char d = 0;
 do
 {
-	printf("Ingrese su día de nacimiento: \n);
+	printf("Ingrese su día de nacimiento: \n");
 	scanf("%s", dia);
 	    if (dia[0] >= 48 && dia[0] <= 50 && dia[1] >= 48 && dia[1] <= 57)
 		       {
-			       printf("Días correctos\n");
+			printf("Días correctos\n");
+	      		d++;
 		       }
 	     else if(dia[0] == 51 && dia[1]<=49)
 	       {
-		       printf("Día correctos\n");
+		printf("Día correctos\n");
+	      	d++;
 	       }
 	     else{
 	     printf("Numero incorrecto\n");
-		     break;
+
 	     }
-	       d++;
 }while(d==0);
 /*MES DE NACIMIENTO*/
-	d==0;
+	d = 0;
 do
 {
-	printf("Ingrese su mes de nacimiento: \n);
+	printf("Ingrese su mes de nacimiento: \n");
 	scanf("%s", mes);
 	    if (mes[0] == 48 && mes[1]>=49 && mes[1]<=57)
 		       {
-			     printf("Mes correcto\n");   
+			printf("Mes correcto\n");   
+	       	      	d++;
 		       }
 	       else if (mes[0] == 49 && mes[1]>=49 && mes[1]<=50)
 	       {
-		       printf("Mes correcto\n");
+		      printf("Mes correcto\n");
+	       	      d++;
 	       }
 	     else{
-	     printf("Mes incorrecto\");
-		     break;
+	     printf("Mes incorrecto\n");
 	     }
-	       d++;
 }while(d==0);
 	
 	
